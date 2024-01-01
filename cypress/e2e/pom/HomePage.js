df1eea03d750a594ca7f63429ebf578eff6ba1c9
Roman Burlaka <burlakarv@gmail.com>
2024-01-01 21:27:53 +0100

    create account

--- cypress/e2e/pom/HomePage.js
+++ cypress/e2e/pom/HomePage.js
@@ -1,5 +1,10 @@
+import CreateAccountPage from "./CreateAccountPage";
+
 class HomePage {
     getHomePage = () => cy.get('img[src="https://magento.softwaretestingboard.com/pub/static/version1695896754/frontend/Magento/luma/en_US/images/logo.svg"]');
+    getCreateAccountButton = () => cy.get('div.panel.wrapper > div ul > li:nth-child(3) > a');
+
+
 
 
     gotoHomePage() {
@@ -7,6 +12,14 @@ class HomePage {
             .click();
 
             return this;
+    }
+
+    clickCreateAccount() {
+        this.getCreateAccountButton()
+            .click();
+      
+
+            return new CreateAccountPage();
     }
 
 }
